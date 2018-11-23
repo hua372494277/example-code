85231871b7573dba41ee037c33031f51d0a45bb4
huayongpan <huayongpan@gmail.com>
2018-11-23 14:27:39 +0800

    Add clock deco 2

---
+++ 07-closure-deco/clockdeco2.py
@@ -0,0 +1,32 @@
+# clockdeco2.py
+
+import time
+import functools
+
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(','.join(pairs))
+        arg_str = ','.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+
+@clock
+def f1(a, b, c = 3):
+    return a + b + c
+
+if __name__ == '__main__':
+    f1(10, 100, c = 1)
+    f1(10, 100, c=1000)
